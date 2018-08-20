2828a8ff42e59a43403ff8563cc4639d39d4d415
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-08-20 11:27:05 -0700

    autogenerate pairs.toml

---
+++ gen_ticker_pairs.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+# script to generate pairs.toml file for the ticker.json using the all assets endpoint
+
+import requests
+
+blacklist_code = ['REMOVE']
+# transform name for asset based on code:issuer whitelist
+name_transforms = {
+    'XCN:GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY': 'CNY',
+    'EURT:GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S': 'EUR',
+    'HKDT:GABSZVZBYEO5F4V5LZKV7GR4SAJ5IKJGGOF43BIN42FNDUG7QPH6IMRQ': 'HKD'
+}
+
+print('''[[pair]]
+name = "EUR_PHP"
+base_asset_code = "EURT"
+base_asset_issuer = "GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S"
+counter_asset_code = "PHP"
+counter_asset_issuer = "GBUQWP3BOUZX34TOND2QV7QQ7K7VJTG6VSE7WMLBTMDJLLAW7YKGU6EP"
+
+[[pair]]
+name = "BTC_XEL"
+base_asset_code = "BTC"
+base_asset_issuer = "GATEMHCCKCY67ZUCKTROYN24ZYT5GK4EQZ65JJLDHKHRUZI3EUEKMTCH"
+counter_asset_code = "XEL"
+counter_asset_issuer = "GAXELY4AOIRVONF7V25BUPDNKZYIVT6CWURG7R2I6NQU26IQSQODBVCS"
+''')
+
+next_url = "https://horizon.stellar.org/assets?limit=200"
+while True:
+    req = requests.get(next_url)
+    o = req.json()
+    next_url = o['_links']['next']['href']
+    records = o['_embedded']['records']
+
+    for r in records:
+        if r['asset_code'] in blacklist_code:
+            continue
+
+        print('[[pair]]')
+
+        # check transform map before printing out name field
+        name_code = r['asset_code']
+        transform_key = r['asset_code'] + ':' + r['asset_issuer']
+        if transform_key in name_transforms:
+            name_code = name_transforms[transform_key]
+
+        print('name = "XLM_' + name_code + '"')
+        print('base_asset_code = "XLM"')
+        print('base_asset_issuer = "native"')
+        print('counter_asset_code = "' + r['asset_code'] + '"')
+        toml_link = r['_links']['toml']['href']
+        if len(toml_link) > 0:
+            print('# toml = ' + toml_link)
+        print('counter_asset_issuer = "' + r['asset_issuer'] + '"')
+        print('')
+
+    # stopping case
+    if len(records) == 0:
+        break
