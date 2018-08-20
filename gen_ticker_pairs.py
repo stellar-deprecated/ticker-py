c43f77610e5feda30a847f3a3dabb892ad19d449
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2018-08-20 15:14:30 -0700

    added token whitelist and expanded asset code transforms

--- gen_ticker_pairs.py
+++ gen_ticker_pairs.py
@@ -16,7 +16,15 @@ blacklist_code = ['REMOVE', 'XLM']
 name_transforms = {
     'XCN:GCNY5OXYSY4FKHOPT2SPOQZAOEIGXB5LBYW3HVU3OWSTQITS65M5RCNY': 'CNY',
     'EURT:GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S': 'EUR',
-    'HKDT:GABSZVZBYEO5F4V5LZKV7GR4SAJ5IKJGGOF43BIN42FNDUG7QPH6IMRQ': 'HKD'
+    'HKDT:GABSZVZBYEO5F4V5LZKV7GR4SAJ5IKJGGOF43BIN42FNDUG7QPH6IMRQ': 'HKD',
+    'WSD:GDSVWEA7XV6M5XNLODVTPCGMAJTNBLZBXOFNQD3BNPNYALEYBNT6CE2V' : 'USD',
+    'USDT:GBOXNWGBB7SG3NVIA7O25M7JIRSXQ4KKU3GYARJEFMQXSR3APF3KRI6S' : 'USD',
+    'NGNT:GAWODAROMJ33V5YDFY3NPYTHVYQG7MJXVJ2ND3AOGIHYRWINES6ACCPD' : 'NGN'
+}
+# always include these token pairs
+whitelist = {
+    'BTC:GDXTJEK4JZNSTNQAWA53RZNS2GIKTDRPEUWDXELFMKU52XNECNVDVXDI': 1,
+    'BTC:GBSTRH4QOTWNSVA6E4HFERETX4ZLSR3CIUBLK7AXYII277PFJC4BBYOG': 1
 }
 
 print('''[[pair]]
@@ -42,27 +50,27 @@ while True:
     records = o['_embedded']['records']
 
     for r in records:
-        if r['asset_code'] in blacklist_code:
-            continue
+        key = r['asset_code'] + ':' + r['asset_issuer']
+        if key not in whitelist:
+            if r['asset_code'] in blacklist_code:
+                continue
 
-        if r['flags']['auth_required']:
-            account_limit = min_account_holders_auth_required
-        else:
-            account_limit = min_account_holders
-        if r['num_accounts'] < account_limit:
-            continue
+            if r['flags']['auth_required']:
+                account_limit = min_account_holders_auth_required
+            else:
+                account_limit = min_account_holders
+            if r['num_accounts'] < account_limit:
+                continue
 
-        if float(r['amount']) < min_issued_assets:
-            continue
+            if float(r['amount']) < min_issued_assets:
+                continue
 
+        # display
         print('[[pair]]')
-
         # check transform map before printing out name field
         name_code = r['asset_code']
-        transform_key = r['asset_code'] + ':' + r['asset_issuer']
-        if transform_key in name_transforms:
-            name_code = name_transforms[transform_key]
-
+        if key in name_transforms:
+            name_code = name_transforms[key]
         print('name = "XLM_' + name_code + '"')
         print('base_asset_code = "XLM"')
         print('base_asset_issuer = "native"')
